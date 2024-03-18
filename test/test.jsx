987fd572c583d1530c611fc326714f795ba118d8
Khaiy1972 <159775242+Khaiy1972@users.noreply.github.com>
2024-03-18 22:46:06 +0800

    done with the timer
    done with the timer and its function

---
+++ test/test.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const topInfo = (
+  <div>
+    <h1>HELLO MADERPAKERS</h1>
+  </div>
+);
+
+ReactDOM.render(topInfo, document.getElementById("app"));
